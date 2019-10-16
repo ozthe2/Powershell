dc764c3455bca7308394f1a59b107041a0e969a4
OzThe2 <ozthe2@users.noreply.github.com>
2019-10-16 14:26:47 +0100

    Create DetectionRule.ps1

---
+++ SCCM/RSAT/DetectionRule.ps1
@@ -0,0 +1,14 @@
+$Components = @('Rsat.Dns.Tools~~~~0.0.1.0','Rsat.GroupPolicy.Management.Tools~~~~0.0.1.0','Rsat.ActiveDirectory.DS-LDS.Tools~~~~0.0.1.0', 'Rsat.DHCP.Tools~~~~0.0.1.0','Rsat.FileServices.Tools~~~~0.0.1.0','Rsat.IPAM.Client.Tools~~~~0.0.1.0', 'Rsat.VolumeActivation.Tools~~~~0.0.1.0','Rsat.CertificateServices.Tools~~~~0.0.1.0')
+ foreach ($Component in $Components) {
+    $InstallState = (Get-WindowsCapability -Name $Component -Online -ErrorAction SilentlyContinue).state
+        if ($InstallState -eq "Installed") {
+            $Result = $true
+        } else {
+            $result = $false
+            break
+        }
+}
+
+if ($result) {
+    write-host "Installed!"
+}
