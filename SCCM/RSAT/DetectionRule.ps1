752cf7df9fce39491147ecd0536eed0038b8a572
OzThe2 <ozthe2@users.noreply.github.com>
2019-10-20 12:09:49 +0100

    add bitlocker

--- SCCM/RSAT/DetectionRule.ps1
+++ SCCM/RSAT/DetectionRule.ps1
@@ -1,4 +1,4 @@
-$Components = @('Rsat.Dns.Tools~~~~0.0.1.0','Rsat.GroupPolicy.Management.Tools~~~~0.0.1.0','Rsat.ActiveDirectory.DS-LDS.Tools~~~~0.0.1.0', 'Rsat.DHCP.Tools~~~~0.0.1.0','Rsat.FileServices.Tools~~~~0.0.1.0','Rsat.IPAM.Client.Tools~~~~0.0.1.0', 'Rsat.VolumeActivation.Tools~~~~0.0.1.0','Rsat.CertificateServices.Tools~~~~0.0.1.0')
+$Components = @('Rsat.Dns.Tools~~~~0.0.1.0','Rsat.GroupPolicy.Management.Tools~~~~0.0.1.0','Rsat.ActiveDirectory.DS-LDS.Tools~~~~0.0.1.0', 'Rsat.DHCP.Tools~~~~0.0.1.0','Rsat.FileServices.Tools~~~~0.0.1.0','Rsat.IPAM.Client.Tools~~~~0.0.1.0', 'Rsat.VolumeActivation.Tools~~~~0.0.1.0','Rsat.CertificateServices.Tools~~~~0.0.1.0','Rsat.BitLocker.Recovery.Tools~~~~0.0.1.0')
  foreach ($Component in $Components) {
     $InstallState = (Get-WindowsCapability -Name $Component -Online -ErrorAction SilentlyContinue).state
         if ($InstallState -eq "Installed") {
