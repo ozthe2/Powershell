d8f06c0daae923111170a13ca8e52c921b5c5bad
OzThe2 <ozthe2@users.noreply.github.com>
2020-02-06 16:56:17 +0000

    Create DetectionRule.ps1

---
+++ SCCM/Mimecast/DetectionRule.ps1
@@ -0,0 +1,42 @@
+$MimecastVersion = '7.8.0.125'
+
+$OfficePath = 'HKLM:\Software\Microsoft\Office'
+$OfficeVersions = @('14.0','15.0','16.0')    
+    
+foreach ($Version in $OfficeVersions) {
+    try {
+        Set-Location "$OfficePath\$Version\Outlook" -ea stop -ev x
+        $LocationSet = $true
+        break
+    } catch {
+        $LocationSet = $false
+    }
+}
+
+
+# Test for O365 ProPlus...
+if (!($LocationSet)) {
+    $OfficePath = 'HKLM:\Software\Microsoft\Office\ClickToRun\Scenario\INSTALL'
+    try {
+        Set-Location $OfficePath -ea stop -ev x
+        $LocationSet = $true
+    } catch {
+        $LocationSet = $false
+    }
+}
+
+
+if ($locationSet) {
+            #Check for bitness and install correct version
+            try {
+                switch (Get-ItemPropertyValue -Name "Bitness" -ea stop) {
+                    "x86" { if ((get-item "${env:ProgramFiles(x86)}\Mimecast\Mimecast Outlook Add-In\en-GB\Mimecast.Services.Outlook.Assets.resources.dll" -ErrorAction SilentlyContinue).VersionInfo.fileversion -eq $MimecastVersion) { Write-host "Installed!"} }
+                    "x64" { if ((get-item "$ENV:ProgramFiles\Mimecast\Mimecast Outlook Add-In\en-GB\Mimecast.Services.Outlook.Assets.resources.dll" -ErrorAction SilentlyContinue).VersionInfo.fileversion -eq $MimecastVersion) { Write-host "Installed!"} }
+                }
+            } catch {
+                switch (Get-ItemPropertyValue -Name "Platform") {
+                    "x86" { if ((get-item "${env:ProgramFiles(x86)}\Mimecast\Mimecast Outlook Add-In\en-GB\Mimecast.Services.Outlook.Assets.resources.dll" -ErrorAction SilentlyContinue).VersionInfo.fileversion -eq $MimecastVersion) { Write-host "Installed!"} }
+                    "x64" { if ((get-item "$ENV:ProgramFiles\Mimecast\Mimecast Outlook Add-In\en-GB\Mimecast.Services.Outlook.Assets.resources.dll" -ErrorAction SilentlyContinue).VersionInfo.fileversion -eq $MimecastVersion) { Write-host "Installed!"} }
+                }
+            }
+}
